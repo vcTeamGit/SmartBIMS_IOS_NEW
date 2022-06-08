fba04cd455ae75084063b8771c8bfe73c658fb30
hmwoo <hmwoo@pcninc.co.kr>
2022-06-08 17:40:59 +0900

    ✅ 테스트를 위한 개발 환경 구축
    공통
    - 접속 시 요청 URL 을 검수 URL 을 바라보도록 설정
    
    로그인화면
    - 텍스트필드에 간편하게 테스트를 하기 위한 접속 아이디 및 비밀번호 미리 입력
    - Z999999Z 로 아이디 입력 시 사파리에서 테스트 바코드를 다운로드 받을 수 있는 링크를 실행하도록 설정
    
    Closes

--- Smart BIMS/SBServerURLContent.m
+++ Smart BIMS/SBServerURLContent.m
@@ -18,7 +18,7 @@ NSString const *BLOOD_SERVER = @"mbims.bloodinfo.net:59999/mbims";
 // 개발 DB 접근 시 TARGET을 DEV로 설정
 // 상용 DB 접근 시 TARGET을 PROD로 설정
 // DEV : 개발, PROD : 상용
-#define TARGET PROD
+#define TARGET DEV
 
 #if TARGET==DEV
     NSString const *SERVER_TARGET = @"/testservice";
